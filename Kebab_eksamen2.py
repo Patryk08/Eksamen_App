328122a9e0c56c18bfa89cea2a8ee5edf34aef73
Patryk08 <patrykmistak@gmail.com>
2026-06-08 13:29:33 +0200

    Update

--- Kebab_eksamen2.py
+++ Kebab_eksamen2.py
@@ -65,8 +65,6 @@ def about():
 def help_page():
     return render_template("help.html")
 
-if __name__ == "__main__":
-    app.run(debug=True)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=6111)
